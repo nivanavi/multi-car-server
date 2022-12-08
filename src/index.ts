dc6277fa88b884be1036140e1d45ec3d7e40c3ae
alipkin <alipkin@tochka.com>
2022-12-08 14:24:37 +0300

    upgrade for support rooms and less necessary messages

--- src/index.ts
+++ src/index.ts
@@ -7,25 +7,30 @@ dotenv.config();
 const port = Number(process.env.PORT);
 
 const wsServer = new WebSocket.Server({port});
-const clients = new Map<WebSocket, { id: string }>();
+const clients = new Map<WebSocket, { carId: string, roomId: string }>();
 
 const connectHandler = (ws: WebSocket) => {
     ws.on('message', rawMessage => {
         const message: WebsocketMessages = JSON.parse(String(rawMessage));
+        const {action, payload: {carId: messageCarId, roomId: messageRoomId}} = message || {};
 
-        switch (message.action) {
+        switch (action) {
             case "CAR_CONNECTED":
-                clients.set(ws, {id: message?.payload?.id});
+                clients.set(ws, {carId: messageCarId, roomId: messageRoomId});
                 [...clients.keys()].forEach(client => {
                     const currentClient = clients.get(client);
-                    if (!currentClient?.id || currentClient.id === message?.payload?.id) return;
+                    const {carId, roomId} = currentClient || {};
+                    if (!carId || !roomId) return;
+                    if (carId === messageCarId || roomId !== messageRoomId) return;
                     client.send(JSON.stringify(message));
                 })
                 break;
             case "CAR_MOVE":
                 [...clients.keys()].forEach(client => {
                     const currentClient = clients.get(client);
-                    if (!currentClient?.id || currentClient.id === message?.payload?.id) return;
+                    const {carId, roomId} = currentClient || {};
+                    if (!carId || !roomId) return;
+                    if (carId === messageCarId || roomId !== messageRoomId) return;
                     client.send(JSON.stringify(message));
                 })
                 break;
@@ -34,15 +39,18 @@ const connectHandler = (ws: WebSocket) => {
 
     ws.on('close', () => {
         const currentWs = clients.get(ws);
-        if (!currentWs?.id) return;
+        const {carId: wsCarId, roomId: wsRoomId} = currentWs || {};
+        if (!wsCarId || !wsRoomId) return;
 
         [...clients.keys()].forEach(client => {
             const currentClient = clients.get(client);
-            if (!currentClient?.id || currentClient.id === currentWs.id) return;
+            const {carId, roomId} = currentClient || {};
+            if (!carId || !roomId) return;
+            if (carId === wsCarId || roomId !== wsRoomId) return;
             client.send(JSON.stringify({
                 action: 'CAR_DELETE',
                 payload: {
-                    id: currentWs.id
+                    carId: wsCarId
                 }
             }));
         })
--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,4 @@
 export type WebsocketMessages = {
 	action: 'CAR_MOVE' | 'CAR_DELETE' | 'CAR_CONNECTED',
-	payload: {id: string}
+	payload: {carId: string, roomId: string}
 }
